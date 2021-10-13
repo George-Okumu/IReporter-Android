f2a4838b048f976efbcab3f1f4079d95b257304f
George Okumu <gokumu12@gmail.com>
2021-10-13 13:24:21 +0300

    added a test

---
+++ app/src/test/java/com/moringa/ireporter/SignUpTest.java
@@ -0,0 +1,32 @@
+package com.moringa.ireporter;
+
+import android.widget.TextView;
+
+import com.moringa.ireporter.ui.SignUpActivity;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricTestRunner;
+
+@RunWith(RobolectricTestRunner.class)
+public class SignUpTest {
+    private SignUpActivity activity;
+    @Before
+    public void setUP(){
+       activity = Robolectric.buildActivity(SignUpActivity.class)
+               .create()
+               .resume()
+               .get();
+
+    }
+
+    @Test
+    public void getTextView() throws Exception {
+        TextView geo = activity.findViewById(R.id.sign_up);
+        Assert.assertTrue("Let's Get Started".equals(geo.getText().toString()));
+    }
+
+}
