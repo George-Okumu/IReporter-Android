e4dc14d9aef95d05a4408635b697d9c8c98784af
PhilipOdhiambo <jofonowino@gmail.com>
2021-10-13 16:32:46 +0300

    User is authenticated before login. If the user is not valid warning message is thrown.

--- app/src/main/java/com/moringa/ireporter/ui/LoginActivity.java
+++ app/src/main/java/com/moringa/ireporter/ui/LoginActivity.java
@@ -2,17 +2,34 @@ package com.moringa.ireporter.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.WindowManager;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.moringa.ireporter.MainActivity;
 import com.moringa.ireporter.R;
+import com.moringa.ireporter.models.JWTAuth;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -24,6 +41,10 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
     ImageView image;
     TextView splatext;
     @BindView(R.id.loginregister) TextView mLoginRegister;
+    @BindView(R.id.loginBtn) TextView mLoginBtn;
+    @BindView(R.id.loginEmail) TextView mEmail;
+    @BindView(R.id.LoginPassword) TextView mPassword;
+    @BindView(R.id.progressBar) ProgressBar mProgressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +53,7 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         setContentView(R.layout.activity_login);
         ButterKnife.bind(this);
         mLoginRegister.setOnClickListener(this);
+        mLoginBtn.setOnClickListener(this);
 
         anima = AnimationUtils.loadAnimation(this, R.anim.animaa);
         animaa = AnimationUtils.loadAnimation(this, R.anim.animaa);
@@ -41,15 +63,75 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
 
         image.setAnimation(animaa);
         splatext.setAnimation(animaa);
+
+
     }
 
     @Override
     public void onClick(View view) {
+
         if (view == mLoginRegister) {
             Intent intent = new Intent(LoginActivity.this, MainActivity.class);
             startActivity(intent);
             finish();
         }
 
+        if (view == mLoginBtn) {
+
+            try {
+                String email = mEmail.getText().toString();
+                String password = mPassword.getText().toString();
+                authenticateUser(email,password);
+                mProgressBar.setVisibility(View.VISIBLE);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+        }
+
+    }
+
+    public  void authenticateUser(String email, String password) throws Exception {
+// Instantiate the RequestQueue.
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String postUrl ="https://ireporter-a.herokuapp.com/account/login";
+
+        JSONObject postData = new JSONObject();
+        postData.put("email", email);
+        postData.put("password", password);
+        // Make post request
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST, postUrl, postData, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                try {
+                    mProgressBar.setVisibility(View.GONE);
+                    if (response.getString("message").equals("Login Successfull")) {
+                        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                        startActivity(intent);
+                        finish();
+                    }
+                } catch (JSONException e) {
+
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                error.printStackTrace();
+                mProgressBar.setVisibility(View.GONE);
+                Log.d("JWT_DECODED","error");
+                AlertDialog alertDialog = new AlertDialog.Builder(LoginActivity.this).create();
+                alertDialog.setTitle("Invalid I-Reporter Credentials");
+                alertDialog.setIcon(R.drawable.warning_24);
+                alertDialog.setMessage("Check email and/or password.");
+
+                alertDialog.setButton(AlertDialog.BUTTON_POSITIVE, "OK", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                    }
+                });
+                alertDialog.show();
+            }
+        });
+        queue.add(jsonObjectRequest);
     }
 }
