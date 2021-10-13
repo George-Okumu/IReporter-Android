dc624af89cbccc6577597dc4d53afd3416250023
George Okumu <gokumu12@gmail.com>
2021-10-13 18:06:39 +0300

    added test example

--- app/src/test/java/com/moringa/ireporter/SignUpTest.java
+++ app/src/test/java/com/moringa/ireporter/SignUpTest.java
@@ -6,13 +6,19 @@ import com.moringa.ireporter.ui.SignUpActivity;
 
 import org.junit.Assert;
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnit;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.mockito.junit.MockitoRule;
 import org.robolectric.Robolectric;
 import org.robolectric.RobolectricTestRunner;
 
 @RunWith(RobolectricTestRunner.class)
 public class SignUpTest {
+
     private SignUpActivity activity;
     @Before
     public void setUP(){
@@ -26,7 +32,7 @@ public class SignUpTest {
     @Test
     public void getTextView() throws Exception {
         TextView geo = activity.findViewById(R.id.sign_up);
-        Assert.assertTrue("Let's G Started".equals(geo.getText().toString()));
+        Assert.assertTrue("Let's Get Started".equals(geo.getText().toString()));
     }
 
 }
