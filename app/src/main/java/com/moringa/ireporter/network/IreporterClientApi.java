21942a3bc1043913aca6150833b7ce49c6b31d6c
PhilipOdhiambo <jofonowino@gmail.com>
2021-10-15 22:04:24 +0300

    commit changes

--- app/src/main/java/com/moringa/ireporter/network/IreporterClientApi.java
+++ app/src/main/java/com/moringa/ireporter/network/IreporterClientApi.java
@@ -1,5 +1,6 @@
 package com.moringa.ireporter.network;
 
+import okhttp3.OkHttpClient;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
@@ -8,12 +9,20 @@ public class IreporterClientApi {
 
     public static String BASE_URL ="https://ireporter-a.herokuapp.com/api/";
     private static Retrofit retrofit = null;
+
+    public static Retrofit getRetrofit() {
+        OkHttpClient okHttpClient  = new OkHttpClient.Builder().build();
+        retrofit = new Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .client(okHttpClient)
+                .build();
+        return retrofit;
+    }
+
     public static IreporterSearchApi getClient(){
         if(retrofit == null){
-            retrofit = new Retrofit.Builder()
-                    .baseUrl(BASE_URL)
-                    .addConverterFactory(GsonConverterFactory.create())
-                    .build();
+
         }
         IreporterSearchApi api = retrofit.create(IreporterSearchApi.class);
         return api;
