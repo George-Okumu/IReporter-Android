84fdfed7a0da867e1710f126c84ef810ff133bc6
PhilipOdhiambo <jofonowino@gmail.com>
2021-10-20 21:38:21 +0300

    remove private for perceler to work

--- app/src/main/java/com/moringa/ireporter/models/RedFlagsResponse.java
+++ app/src/main/java/com/moringa/ireporter/models/RedFlagsResponse.java
@@ -11,7 +11,7 @@ import java.util.List;
 public class RedFlagsResponse {
     @SerializedName("redflags")
     @Expose
-    private List<RedFlag> redFlags = null;
+    List<RedFlag> redFlags = null;
 
     //Default Cosntructor
     public RedFlagsResponse() {
